6ced7f0db5ed1542ddbc6b063f8444bb5b3ffdca
William Key <me@willclarke.net>
2025-04-04 11:30:14 -0700

    Initial Commit

---
+++ DragMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragMatch
+//
+//  Created by William Key on 4/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragMatch/DragMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragMatchApp.swift
+//  DragMatch
+//
+//  Created by William Key on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DragMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
